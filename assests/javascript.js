11f149e9194a986ff0fbac675a14db877dd5fb6a
Asmaa <Asmaa99musse@gmail.com>
2021-10-08 01:04:38 +0100

    added comments planned structure for js

--- assests/javascript.js
+++ assests/javascript.js
@@ -1,1 +1,25 @@
 const questions = document.getElementById(questions);
+
+// Create an array for question
+
+// Create function to start game
+
+// First question
+
+// validate answer - boolean
+
+// Display correct answer
+
+// CREATE 2 FUNCTION TO DISPLAY
+// WRONG!
+// CORRECT
+
+// Move to the next question
+
+// End game when time runs out
+
+// Display highScore
+
+// keep memory of game scores
+
+// Display highScore when nav link is clicked
