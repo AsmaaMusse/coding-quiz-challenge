eda74a46947ae7d38a350f196e5c17b6f2e3975a
Asmaa <Asmaa99musse@gmail.com>
2021-10-08 20:26:22 +0100

    Added quiz question

--- assests/javascript.js
+++ assests/javascript.js
@@ -2,6 +2,40 @@ const questions = document.getElementById(questions);
 const highScore = document.getElementById(highScore);
 const timer = document.getElementById(timer);
 
+// quiz questions:
+
+// 1. What does CSS stand for?
+// A. Creative Style Sheets
+// B. Compact Style Sheets
+// C. Cascading Style Sheets -->
+
+// 2. Choose the correct HTML tag for the largest heading
+// A. <heading>
+// B. <h6>
+// C. <head>
+// D. <h1></h1> -->
+
+// 3. Which is the correct code for a relative hyperlink?
+// A. < a href= " ../index.html " > Link < /a > -->
+// B. Www.index.com
+// C. < a href= " html.index.com " > Link < /a >
+// D. < a href= " www.index.com " > Link < /a >
+
+// 4. Where is the correct place to insert a JavaScript?
+// A. the <body> section
+// B. The <head> section
+// C. Both the <head> section and the <body> section are correct -->
+
+// 5. How do you create a function in JavaScript?
+// A. function myFunction{} -->
+// B. function = myFunction()
+// C. function:myFunction()
+
+// 6. How does a WHILE loop start?
+// A. while i = 1 to 10
+// B. while (i<=10) -->
+// C. while (i<=10;i ++)
+
 // Create an array for question
 
 // Create function to start game
