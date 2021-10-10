4237eed6caa164ce9b9b9f396c166ff9db2e7caa
Asmaa <Asmaa99musse@gmail.com>
2021-10-10 23:36:13 +0100

    working in the next question btn

--- assests/javascript/javascript.js
+++ assests/javascript/javascript.js
@@ -28,27 +28,46 @@ continueBtn.onclick = () => {
   const questionSection = document.querySelector(".questions-section");
   questionSection.classList.add("activeQuestion");
   // Shows Questions page
-  showQuestions(1);
+  showQuestions(0);
 };
 
 // Create a questions array -> other js page
 
 let questionCount = 0;
 
+// When Next Btn is clicked
+const nextBtn = document.querySelector(".next-question");
+
+nextBtn.onclick = () => {
+  questionCount++;
+  showQuestions(questionCount);
+};
+
 // Fetching questions from the array
 function showQuestions(index) {
   const questionText = document.querySelector("question-text");
   const optionList = document.querySelector(".option-list");
+
+  questionText.innerHTML = questionTag;
+  optionList.innerHTML = optionTag;
   let questionTag = "<span>" + questions[index].question + "</span>";
   let optionTag =
     '<div class="option">' +
     question[index].options[0] +
     "<span></span></div>" +
     '<div class="option">' +
-    question[index].options[0] +
+    question[index].options[1] +
     "<span></span></div>" +
     '<div class="option">' +
-    question[index].options[0] +
+    question[index].options[2] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    question[index].options[3] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    question[index].options[4] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    question[index].options[5] +
     "<span></span></div>";
-  questionText.innerHTML = questionTag;
 }
