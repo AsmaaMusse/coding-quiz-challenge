16b34cbad302721d4a42f47beec69f383b26117e
Asmaa <Asmaa99musse@gmail.com>
2021-10-11 19:59:54 +0100

    restyling quiz cards

--- assests/javascript/javascript.js
+++ assests/javascript/javascript.js
@@ -1,5 +1,4 @@
 const startBtn = document.querySelector(".start-btn button");
-const nextQuestionBtn = document.querySelector(".next-question");
 const restartBtn = document.querySelector(".buttons .restart");
 const quitBtn = document.querySelector(".buttons .quit");
 const starterContainer = document.querySelector(".starter-container");
@@ -26,48 +25,16 @@ continueBtn.onclick = () => {
   // Also hides info box
 
   const questionSection = document.querySelector(".questions-section");
-  questionSection.classList.add("activeQuestion");
-  // Shows Questions page
-  showQuestions(0);
+  questionSection.classList.add("activeQuestion"); // Shows Questions page
 };
 
 // Create a questions array -> other js page
 
-let questionCount = 0;
-
-// When Next Btn is clicked
-const nextBtn = document.querySelector(".next-question");
-
-nextBtn.onclick = () => {
-  questionCount++;
-  showQuestions(questionCount);
-};
-
-// Fetching questions from the array
-function showQuestions(index) {
-  const questionText = document.querySelector("question-text");
-  const optionList = document.querySelector(".option-list");
-
-  questionText.innerHTML = questionTag;
-  optionList.innerHTML = optionTag;
-  let questionTag = "<span>" + questions[index].question + "</span>";
-  let optionTag =
-    '<div class="option">' +
-    question[index].options[0] +
-    "<span></span></div>" +
-    '<div class="option">' +
-    question[index].options[1] +
-    "<span></span></div>" +
-    '<div class="option">' +
-    question[index].options[2] +
-    "<span></span></div>" +
-    '<div class="option">' +
-    question[index].options[3] +
-    "<span></span></div>" +
-    '<div class="option">' +
-    question[index].options[4] +
-    "<span></span></div>" +
-    '<div class="option">' +
-    question[index].options[5] +
-    "<span></span></div>";
+// Get the question container
+const questionSection = function() {
+  // 
 }
+
+
+
+const currentQuestion = questions[0]
--- assests/javascript/questions.js
+++ assests/javascript/questions.js
@@ -1,64 +1,50 @@
 // Create a questions array
 
-let questions = [
-    {
-      Number: 1,
-      question: "What does CSS stand for?",
-      answer: "Cascading Style Sheets",
-      options: [
-        "Creative Style Sheets",
-        "Compact Style Sheets",
-        "Cascading Style Sheets",
-      ],
-    },
-  
-    {
-      Number: 2,
-      question: "Choose the correct HTML tag for the largest heading",
-      answer: "<h1></h1>",
-      options: ["<heading>", "<head>", "<h1></h1>"],
-    },
-  
-    {
-      Number: 3,
-      question: "Which is the correct code for a relative hyperlink?",
-      answer: "< a href= " ../index.html " > Link < /a >",
-      options: [
-        "< a href= " ../index.html " > Link < /a >",
-        "< a href= " html.index.com " > Link < /a >",
-        "< a href= " www.index.com " > Link < /a >"
-      ],
-    },
-  
-    {
-      Number: 4,
-      question: "Where is the correct place to insert a JavaScript?",
-      answer: "Both the <head> section and the <body> section are correct",
-      options: [
-        "The <body> section",
-        "The <head> section",
-        "Both the <head> section and the <body> section are correct",
-      ],
-    },
-  
-    {
-      Number: 5,
-      question: "How do you create a function in JavaScript?",
-      answer: "function myFunction{}",
-      options: [
-        "function myFunction{}",
-        "function myFunction{}",
-        "function:myFunction()",
-      ],
-    },
-    {
-      Number: 6,
-      question: "How does a WHILE loop start?",
-      answer: "while (i<=10)",
-      options: [
-        "while i = 1 to 10",
-        "while (i<=10)",
-        "while (i<=10;i ++)",
-      ],
-    },
-  ];
+const questions = [
+  {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Creative Style Sheets",
+      "Compact Style Sheets",
+      "Cascading Style Sheets",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "Choose the correct HTML tag for the largest heading",
+    answer: "<h1></h1>",
+    options: ["<heading>", "<head>", "<h1></h1>"],
+  },
+
+  {
+    numb: 3,
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "Both the <head> section and the <body> section are correct",
+    options: [
+      "The <body> section",
+      "The <head> section",
+      "Both the <head> section and the <body> section are correct",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "How do you create a function in JavaScript?",
+    answer: "function myFunction{}",
+    options: [
+      "function myFunction{}",
+      "function myFunction{}",
+      "function:myFunction()",
+    ],
+  },
+
+  {
+    numb: 5,
+    question: "How does a WHILE loop start?",
+    answer: "while (i<=10)",
+    options: ["while i = 1 to 10", "while (i<=10)", "while (i<=10;i ++)"],
+  },
+];
